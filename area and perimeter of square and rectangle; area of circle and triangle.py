acda550e084ed4d9b7ae823cfb5f3be416a059be
Meetchauhan07 <meet.chauhan.mk@gmail.com>
2025-03-17 23:24:55 +0530

    Add files via upload

---
+++ area and perimeter of square and rectangle; area of circle and triangle.py
@@ -0,0 +1,47 @@
+#area & perimeter of a square
+side=int(input("Enter the side length="))
+area=side*side
+perimeter=side*4
+print('Area is',area,'and perimeter is',perimeter)
+'''
+output=
+Enter the side length=5
+Area is 25 and perimeter is 20
+'''
+#area & perimeter of a rectangle
+l=int(input("Enter the length="))
+b=int(input("Enter the breadth ="))
+area=l*b
+perimeter=2*(l+b)
+print('area is',area,'perimeter is',perimeter)
+'''
+output=
+Enter the length=8
+Enter the breadth =6
+area is 48 perimeter is 28
+'''
+
+#area of cicle
+radius=int(input("Enter the radius="))
+area=(22/7)*(radius*radius)
+print('area of circle is',area)
+'''
+output
+Enter the radius=7
+area of circle is 154.0
+
+'''
+#area of triangle
+base=int(input("Enter the base="))
+height=int(input("Enter the height="))
+area=0.5*base*height
+print('area of triangle is',area)
+'''
+output=
+Enter the base=56
+Enter the height=78
+area of triangle is 2184.0
+
+'''
+
+
