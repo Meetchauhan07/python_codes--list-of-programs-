4196c4d1ad372f1cadd2f0ff855f4bd9c8e07b76
Meetchauhan07 <meet.chauhan.mk@gmail.com>
2025-03-17 23:02:54 +0530

    Add files via upload

---
+++ Calculating simple interest.py
@@ -0,0 +1,14 @@
+#Calculaing interest where I = PRN/100
+principle_amount=float(input("Enter the Principle amount="))
+rate_of_interest=float(input("Enter the  rate of interest="))
+time_period=float(input("Enter the no. of years="))
+i=(principle_amount*rate_of_interest*time_period)/100
+print('interest is ',i)
+'''
+output=
+Enter the Principle amount=5000
+Enter the  rate of interest=5
+Enter the no. of years=2
+interest is  500.0
+
+'''
