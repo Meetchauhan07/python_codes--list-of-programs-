93b19feaefc2fa93e6d830c611c9fd25a9be204b
Meetchauhan07 <meet.chauhan.mk@gmail.com>
2025-03-17 17:18:25 +0530

    Add files via upload

---
+++ minutes and hours conversion.py
@@ -0,0 +1,20 @@
+#Convert hours to minutes
+hours = float(input("Enter hours: "))
+minutes = hours * 60  
+print(hours,' hours is equal to ',minutes,' minutes.')
+'''
+output=
+Enter hours: 5
+5.0  hours is equal to  300.0  minutes.
+'''			
+
+
+#convert minutes to hours
+min=float(input("Enter minutes: "))
+hrs=min/60
+print(min,' minutes is equal to ',hrs,' hours.')
+'''
+output=
+Enter minutes: 150
+150.0  minutes is equal to  2.5  hours.
+'''
