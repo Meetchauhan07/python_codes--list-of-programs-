9fb7b504e27352e0f1755e313075572a532dfcda
Meetchauhan07 <meet.chauhan.mk@gmail.com>
2025-03-17 17:44:09 +0530

    Add files via upload

---
+++ rupees,dollars,pounds.py
@@ -0,0 +1,34 @@
+#convert dollars to ruppes
+dollars=float(input("Enter amount in dollars: "))
+ruppes=dollars*48
+print(dollars,'dollars is equal to rupees',ruppes)
+'''
+output=
+Enter amount in dollars: 78
+78.0 dollars is equal to rupees 3744.0
+
+'''
+
+#convert ruppes to dollars
+ruppes=float(input("Enter amount in ruppes"))
+dollars=ruppes/48
+print( dollars, "dollars is equal to ruppes", ruppes)
+
+'''
+output=
+Enter amount in ruppes960
+20.0 dollars is equal to ruppes 960.0
+
+'''
+
+#dollars to pounds
+dollars=float(input("Enter amount in dollars: "))
+ruppes=48*dollars
+pounds=ruppes/70
+print(dollars,'dollars is equal to pounds',pounds)
+
+'''
+output=
+Enter amount in dollars: 2800
+2800.0 dollars is equal to pounds 1920.0
+'''
