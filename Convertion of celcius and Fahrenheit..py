6ea39109a290c4677ed289fb3a234d5fe6b496c6
Meetchauhan07 <meet.chauhan.mk@gmail.com>
2025-03-17 22:51:52 +0530

    Add files via upload

---
+++ Convertion of celcius and Fahrenheit..py
@@ -0,0 +1,20 @@
+#Convert celcius into Fahrenheit.
+celsius=float(input("Enter temperature in Celsius:"))
+fahrenheit=(celsius * 9/5) + 32
+print(celsius,'degree celsius is equal to ',fahrenheit,'fahrenheit')
+'''
+output=
+Enter temperature in Celsius:50
+50.0 degree celsius is equal to  122.0 fahrenheit
+
+'''
+#Convert Fahrenheit into celsius
+fahrenheit=float(input("Enter temperature in fahrenheit:"))
+celsius=(fahrenheit-32)*9/5
+print(celsius,'degree celsius is equal to ',fahrenheit,'fahrenheit')
+'''
+output=
+Enter temperature in fahrenheit:52
+36.0 degree celsius is equal to  52.0 fahrenheit
+
+'''
