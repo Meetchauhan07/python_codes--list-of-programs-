86a6551fe1b24d81676dacff366e00d3f0d5b85a
Meetchauhan07 <meet.chauhan.mk@gmail.com>
2025-03-17 17:58:32 +0530

    Add files via upload

---
+++ conversion between gram and kilogram.py
@@ -0,0 +1,19 @@
+#conver grams to kg
+gram=float(input("Enter weight in grams:"))
+kg=gram/1000
+print(gram,'grams is equal to',kg,'kilogram')
+'''
+output=
+Enter weight in grams:6700
+6700.0 grams is equal to 6.7 kilogram
+
+'''
+#convert kg to grams
+kg=float(input("Enter weight in kilograms:"))
+gram=kg*1000
+print(gram,'grams is equal to',kg,'kilograms')
+'''
+output=
+Enter weight in kilograms:5
+5000.0 grams is equal to 5.0 kilograms
+'''
