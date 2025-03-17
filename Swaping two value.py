2a7631446fc4e0e81562225dcccab5334228f5fe
Meetchauhan07 <meet.chauhan.mk@gmail.com>
2025-03-18 00:05:52 +0530

    Add files via upload

---
+++ Swaping two value.py
@@ -0,0 +1,16 @@
+#Swaping two value
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+print('Before swapping: a=',a,',b=',b)
+temp=a
+a=b
+b=temp
+print('After swapping: a=',a,',b=',b)
+'''
+output=
+Enter the first number: 514
+Enter the second number: 163
+Before swapping: a= 514 ,b= 163
+After swapping: a= 163 ,b= 514
+
+'''
